7a660eed5cb621e19592c055720e719b10ae3b8d
Michael Hope <hopemichael720@gmail.com>
2021-11-08 17:09:39 +0000

    commit

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,5 +1,7 @@
-import java.text.SimpleDateFormat
-import java.util.Locale
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
 
 
 object HelloWorld {
@@ -9,7 +11,10 @@ object HelloWorld {
 
     println(toFahrenheit(23.5))
 
-    println(formatDate("21/09/1926"))
+    println(formatDate())
+
+    println(regexes("root:x:0:0:/root:/bin/bash"))
+
   }
 
   // Q1. function to convert celsius to fahrenheit
@@ -18,11 +23,31 @@ object HelloWorld {
 
 
   // Q2.
-  def formatDate(sourceDate : String): String = {
-    val originalFormat = new SimpleDateFormat("dd/mm/yyyy", Locale.ENGLISH)
-    val targetFormat = new SimpleDateFormat("dd MMMM yyyy")
-    val date = originalFormat.parse(sourceDate)
-    return targetFormat.format(date)
+  def formatDate(): String = {
+//    val originalFormat = new SimpleDateFormat("dd/mm/yyyy", Locale.ENGLISH)
+//    val targetFormat = new DateTimeFormatter("dd MMMM yyyy")
+//    val date = originalFormat.parse(sourceDate)
+//    targetFormat.format(date)
+
+    import java.time.LocalDate
+    val date = LocalDate.now()
+
+    val customFormatter = DateTimeFormatter.ofPattern("dd MMMM yyyy")
+    val text = customFormatter.format(date)
+
+    text
+  }
+
+  def regexes(linux_pass :String) = {
+    val linux_passwords = raw"(\S+):(\S):(\d):(\d):/(\S+):/(\S+)/(\S+)".r
+
+    linux_pass match {
+      case linux_passwords(username, password, userid, groupid, description,
+      home_directory, shell) =>
+        s"username : $username, password: $password, userid: $userid, groupid: $groupid," +
+          s"description: $description, home_dir: $home_directory, shell: $shell"
+    }
+
   }
 
 }
